ae073d3826a8185f04d363df40e006df858bb930
Yuki-Inagawa <yuki2012mu@gmai.com>
2019-08-30 16:16:19 +0900

    destroyメソッドの定義

--- app/controllers/ikezakis_controller.rb
+++ app/controllers/ikezakis_controller.rb
@@ -1,5 +1,5 @@
 class IkezakisController < ApplicationController
-  before_action :move_to_index, except: [:index, :list]
+  before_action :move_to_signup, except: [:index, :list]
 
   def index
     @ikezakis = Ikezaki.all
@@ -25,11 +25,19 @@ class IkezakisController < ApplicationController
     @ikezakis = Ikezaki.all
   end
 
+  def destroy
+    ikezaki = Ikezaki.find(params[:id])
+    if ikezaki.user_id == current_user.id
+      ikezaki.destroy
+    end
+  end
+
   private
   def create_params
     params.require(:ikezaki).permit(:name, :occupation, :love, :birth1, :birth2, :birth3, :grand_prix, :height, :weight, :salary,:balance, :password, :tip, :sex).merge(user_id: current_user.id)
   end
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
+  def move_to_signup
+    redirect_to controller: 'devise/sessions', action: 'new' unless user_signed_in?
+    flash[:notice] = "ログインまたは新規登録をお願いします"
   end
 end
